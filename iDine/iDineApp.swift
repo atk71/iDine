a260313f30b7456f3ebfef63f48625a21c07594e
Andrew Kuttichira <85650912+atk71@users.noreply.github.com>
2021-11-22 01:53:25 -0500

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Andrew Kuttichira on 11/22/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
